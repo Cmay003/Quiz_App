ec34d20106986d40049000e3afa1bf945269e3a4
Soumya Bhakuni <soumyabhakuni456@gmail.com>
2025-02-04 00:03:11 +0530

    Update Ques.css and Ques.jsx with necessary changes

--- src/Ques.jsx
+++ src/Ques.jsx
@@ -1,17 +1,25 @@
-import "./Ques.css"
+import "./Ques.css";
+
 export default function Ques() {
   return (
     <div>
+        <p className="Timer">10:00</p>
         <div className="ques-container">
-            <p className="Timer">10:00</p>
             <div className="ques-box">
                 <h1>Question 1:</h1>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi perspiciatis cupiditate voluptate eum dolor, ut quos voluptatem impedit cumque eveniet.</p>
             </div>
-            <button className="next-btn" >
-                Next
-            </button>
+            
+            {/* Options grid */}
+            <div className="options-container">
+                <button className="option-btn">Option 1</button>
+                <button className="option-btn">Option 2</button>
+                <button className="option-btn">Option 3</button>
+                <button className="option-btn">Option 4</button>
+            </div>
+            
+            <button className="next-btn">Next</button>
         </div>
     </div>
-  )
+  );
 }
